73239951ec8cb0b41235e6d6934530cb6c1af1c7
Cassio Libraga <cassiolib@gmail.com>
2017-11-26 01:21:49 -0200

    Custom Glide

--- prado/src/main/java/com/schibsted/spain/prado/imageprovider/GlideImageProvider.kt
+++ prado/src/main/java/com/schibsted/spain/prado/imageprovider/GlideImageProvider.kt
@@ -1,10 +1,23 @@
 package com.schibsted.spain.prado.imageprovider
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.os.Build
+import android.os.Environment
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.util.Log
 import android.widget.ImageView
+import android.widget.RelativeLayout
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.animation.GlideAnimation
+import com.bumptech.glide.request.target.BitmapImageViewTarget
 import com.github.chrisbanes.photoview.PhotoViewAttacher
 import com.schibsted.spain.prado.R
+import java.io.*
 
 
 class GlideImageProvider : ImageProvider {
@@ -18,11 +31,82 @@ class GlideImageProvider : ImageProvider {
   }
 
   override fun loadImage(context: Context, imageUrl: String, imageView: ImageView) {
-    Glide.with(context)
-        .load(imageUrl)
-        .error(R.drawable.nophoto)
-        .placeholder(R.drawable.placeholder)
-        .thumbnail(IMAGE_QUALITY_1_PERCENT)
-        .into(GlideDrawablePhotoViewAttacherTarget(imageView, PhotoViewAttacher(imageView)))
+
+    if (verifyIfImageExists(imageUrl)) {
+
+      val file = File(pathToSDCard(), imageUrl)
+
+      Glide.with(context)
+          .load(file)
+          .error(R.drawable.nophoto)
+          .placeholder(R.drawable.placeholder)
+          .thumbnail(IMAGE_QUALITY_1_PERCENT)
+          .diskCacheStrategy(DiskCacheStrategy.NONE)
+          .skipMemoryCache(true)
+          .into(GlideDrawablePhotoViewAttacherTarget(imageView, PhotoViewAttacher(imageView)))
+
+    }
+    else {
+
+      Glide.with(context)
+          .load(imageUrl)
+          .asBitmap()
+          .error(R.drawable.nophoto)
+          .placeholder(R.drawable.placeholder)
+          .thumbnail(IMAGE_QUALITY_1_PERCENT)
+          .diskCacheStrategy(DiskCacheStrategy.NONE)
+          .skipMemoryCache(true)
+          .into(object: BitmapImageViewTarget(imageView) {
+
+          override fun onResourceReady(resource: Bitmap?, glideAnimation: GlideAnimation<in Bitmap>?) {
+            saveImage(resource, imageUrl)
+            imageView.scaleType = ImageView.ScaleType.MATRIX
+            imageView.layoutParams = RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT)
+            val photoViewAttacher = PhotoViewAttacher(imageView)
+            photoViewAttacher.update()
+            super.onResourceReady(resource, glideAnimation)
+
+          }
+      })
+    }
+
   }
+
+  fun verifyIfImageExists(file: String) : Boolean {
+    val file = File(pathToSDCard(), file)
+    Log.d("File", file.toString())
+    if (file.exists()) {
+      return true
+    }
+
+    return false
+  }
+
+  fun pathToSDCard() : String {
+    return Environment.getExternalStorageDirectory().toString() + "/ReyTemper/"
+  }
+
+  fun saveImage(resource: Bitmap?, file: String) {
+
+    val file = File(pathToSDCard(), file)
+    val dir = file.parentFile
+    try {
+      if (!dir.mkdirs() && (!dir.exists() || !dir.isDirectory())) {
+        throw IOException("Cannot ensure parent directory for file " + file)
+      }
+
+      val s = BufferedOutputStream(FileOutputStream(file))
+
+      val stream = ByteArrayOutputStream()
+      resource!!.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+      val byteArray = stream.toByteArray()
+
+      s.write(byteArray)
+      s.flush()
+      s.close()
+    } catch (e: IOException) {
+      e.printStackTrace()
+    }
+  }
+
 }
