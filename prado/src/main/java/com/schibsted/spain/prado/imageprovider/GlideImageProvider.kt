85a4477e24e550e1fdf468a792dee418cc41f1e2
Cassio Libraga <cassiolib@gmail.com>
2017-12-29 02:04:33 -0200

    Fix file exists

--- prado/src/main/java/com/schibsted/spain/prado/imageprovider/GlideImageProvider.kt
+++ prado/src/main/java/com/schibsted/spain/prado/imageprovider/GlideImageProvider.kt
@@ -71,7 +71,7 @@ class GlideImageProvider : ImageProvider {
   fun verifyIfImageExists(file: String) : Boolean {
     val file = File(pathToSDCard(), file)
     Log.d("File", file.toString())
-    Log.d("Exite", file.exists())
+    Log.d("Exite", file.exists().toString())
     return file.exists()
   }
 
